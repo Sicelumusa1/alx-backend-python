e228acaef588735010cf565da8c82398ed2fc13c
Sicelumusa <musaqwabe@gmail.com>
2024-03-18 10:41:03 +0000

    added all projects files

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -22,7 +22,7 @@ async def measure_time(n: int, max_delay: int) -> float:
     start_time = time.time()
     await wait_n(n, max_delay)
     end_time = time.time()
-    
+
     total_time = end_time - start_time
     average_time = total_time / n
     return average_time
--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -1,16 +1,19 @@
 #!/usr/bin/env python3
 
 """
-Creates an asyncio.task that wait for a random delay between 0 and specified max_delay
+Creates an asyncio.task that wait for a random delay between 0
+and specified max_delay
 """
 
 import asyncio
 from typing import Any
 wait_random = __import__('0-basic_async_syntax').wait_random
 
+
 def task_wait_random(max_delay: int) -> Any:
     """
-    Creates an asyncio.task that wait for a random delay between 0 and    specified max_delay
+    Creates an asyncio.task that wait for a random delay between 0
+    and specified max_delay
 
     Args:
         max_delay (int): maimum delay in seconds

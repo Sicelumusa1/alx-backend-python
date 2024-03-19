47212f16e758f4db297629c266e2c7c39aab5346
Sicelumusa <musaqwabe@gmail.com>
2024-03-19 12:38:39 +0000

    changed the return type

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -10,7 +10,7 @@ from typing import Any
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-def task_wait_random(max_delay: int) -> Any:
+def task_wait_random(max_delay: int) -> asyncio.Task:
     """
     Creates an asyncio.task that wait for a random delay between 0
     and specified max_delay

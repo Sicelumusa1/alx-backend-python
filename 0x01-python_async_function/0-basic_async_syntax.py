eb8f9367f097957d50b8e49017f813aa04623770
Sicelumusa <musaqwabe@gmail.com>
2024-03-19 12:47:07 +0000

    Resolved pycodestyle issues

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -7,13 +7,15 @@ Defines a basic asynchronous coroutine
 import asyncio
 import random
 
+
 async def wait_random(max_delay: int = 10) -> float:
     """
-    Asynchronous coroutine that waits for a random delay between 0 and max_delay(10)
+    Asynchronous coroutine that waits for a random
+    delay between 0 and max_delay(10)
     Args:
         int: maximum delay
     Returns:
-        float: random delay 
+        float: random delay
     """
     delay = random.uniform(0, max_delay)
     await asyncio.sleep(delay)
